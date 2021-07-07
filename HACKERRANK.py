6dd6ad4fa4e47fb8bce8d358899aab410d33266f
Rohit Kumar <rk8446383@gmail.com>
2021-07-07 13:58:36 +0530

    Update HACKERRANK.py

--- HACKERRANK.py
+++ HACKERRANK.py
@@ -320,3 +320,24 @@ import re   #input 100,000,000.000
 # for i in range(thickness):
 #     print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
 # ===========================================================================================
+# THE MINION GAME
+# def minion_game(string):
+#     player1 = 0
+#     player2 = 0
+#     str_len = len(string)
+#     for i in range(str_len):
+#         if s[i] in "AEIOU":
+#             player1 += (str_len) - i
+#         else:
+#             player2 += (str_len) - i
+# 
+#     if player1 > player2:
+#         print("Kevin", player1)
+#     elif player1 < player2:
+#         print("Stuart", player2)
+#     elif player1 == player2:
+#         print("Draw")
+# 
+# if __name__ == '__main__':
+#     s = input()
+#     minion_game(s)
