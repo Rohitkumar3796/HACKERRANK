fa04d163c8db17e867ac25548e6d448b6dcf28fa
Rohit Kumar <rk8446383@gmail.com>
2021-07-26 19:55:15 +0530

    Update HACKERRANK.py

--- HACKERRANK.py
+++ HACKERRANK.py
@@ -359,3 +359,13 @@ import re   #input 100,000,000.000
 #                         return "not Found"
             
 # print(addTwoElements_([2,7,11,15],9))
+# =============================================================================================
+# leftRotation
+# def rotateLeft(d, arr):
+#     for i in range(0,d):
+#         rotateValue=arr[0]
+#         for j in range(0,len(arr)-1):
+#             arr[j]=arr[j+1]
+#         arr[len(arr)-1]=rotateValue
+#     return arr
+# rotateLeft(3,[1,2,3,4,5])
